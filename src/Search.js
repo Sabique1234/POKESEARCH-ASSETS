0b56101f2bfb0d68bce69cab5df8e8e9e86d9ef5
Sabique1234 <66312211+Sabique1234@users.noreply.github.com>
2020-12-27 13:05:40 +0530

    Add files via upload

---
+++ src/Search.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class Search extends React.Component{
+  render(){
+      return(
+        <View style={styles.container}>
+        <Text style={styles.textStyle}>SEARCH HERE...</Text>
+
+        </View>
+      )
+  }
+}
+
+const styles={
+
+    container:{
+        flex:1
+    },
+
+    textStyle:{
+        marginTop:200,
+        marginLeft:200,
+        color:'orange',
+        width:20,
+        height:20
+    }
+
+}
+
+export default Search;
